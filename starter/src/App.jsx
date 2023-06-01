8620955871f2c75f79921eed27e93bf452d2d224
Justin Jones <jus.jones@yahoo.com>
2023-06-01 14:33:04 -0400

    added a button

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -6,13 +6,24 @@ const App = () => {
   const [people, setPeople] = useState(data);
   // console.log(people);
 
+  const handleButtonClick = () =>{
+    console.log("working");
+    setPeople([]);
+  }
 
-  return <main>
-   <section className="container">
-     <h3>{people.length} birthdays today</h3>
-     <List people={people} />
-   </section>
-    
-    </main>;
+  return (
+  
+    <main>
+      <section className="container">
+      <h3>{people.length} birthdays today</h3>
+       <List people={people} />
+      </section>
+      <button 
+        type="button" 
+        className="btn btn-block"
+        onClick={handleButtonClick}
+        > clear all</button>
+    </main> 
+    );
 };
 export default App;
